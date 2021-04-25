b1e279e74f8196ff1a09bba132e2c991c04843fa
Gabriel Ungier <gungier1@gmail.com>
2021-04-25 14:46:03 -0400

    working on photo gallery carousel

--- bkcarcare/src/components/Pages/PhotoGallery.js
+++ bkcarcare/src/components/Pages/PhotoGallery.js
@@ -1,8 +1,13 @@
-import React from 'react'; 
-//import BKphotos from 'b&kShopPhotos';
-
-function Photos (){
-
+//import React from 'react'
+//import Photos from './GallerySlider'
+/**
+const PhotoGallery = () => {
+    return (
+        <div>
+            Images
+        </div>
+    )
 }
 
-export default Photos;
+export default PhotoGallery
+ */
